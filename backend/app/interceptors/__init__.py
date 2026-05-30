558f55fa3ba3109bfd7c289979fc08ccffe08895
nikhilvashisht <nikhilvashisht50@gmail.com>
2026-05-30 19:04:35 +0530

    directory structure

--- backend/app/interceptors/__init__.py
+++
@@ -1,9 +0,0 @@
-from .openai import OpenAIInterceptor
-from .anthropic import AnthropicInterceptor
-from .google import GoogleGenAIInterceptor
-
-__all__ = [
-    "OpenAIInterceptor",
-    "AnthropicInterceptor",
-    "GoogleGenAIInterceptor"
-]
